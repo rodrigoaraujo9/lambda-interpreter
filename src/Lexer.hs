ddff40dd4b6f9003883da6a875d63a5bb46253af
Rodrigo Araújo <rodrigogomesaraujo04@gmail.com>
2026-03-05 14:41:14 +0000

    asthetics :)

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -8,15 +8,15 @@ lexer (c:cs)
     | isSpace c = lexer cs
     | isAlpha c = lexWord (c:cs)
     | isDigit c = lexNum (c:cs)
-lexer ('=':cs) = TEq      : lexer cs
-lexer ('+':cs) = TPlus    : lexer cs
-lexer ('-':cs) = TMinus   : lexer cs
-lexer ('*':cs) = TTimes   : lexer cs
-lexer ('(':cs) = TLParen  : lexer cs
-lexer (')':cs) = TRParen  : lexer cs
-lexer ('.':cs) = TDot     : lexer cs
-lexer ('\\':cs)= TBackslash : lexer cs
-lexer (c:_)    = error $ "Unexpected character: " ++ [c]
+lexer ('=':cs)  = TEq      : lexer cs
+lexer ('+':cs)  = TPlus    : lexer cs
+lexer ('-':cs)  = TMinus   : lexer cs
+lexer ('*':cs)  = TTimes   : lexer cs
+lexer ('(':cs)  = TLParen  : lexer cs
+lexer (')':cs)  = TRParen  : lexer cs
+lexer ('.':cs)  = TDot     : lexer cs
+lexer ('\\':cs) = TBackslash : lexer cs
+lexer (c:_)     = error $ "Unexpected character: " ++ [c]
 
 lexNum :: String -> [Token]
 lexNum cs = TInt (read num) : lexer rest
